3872f21bd4b5e64c6ac2843c006158dce0e6d27e
Danny Kim <dudkny@gmail.com>
2019-09-19 09:28:31 -0500

    Added and ran SQL for setup

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Bacon Cheeseburger', FALSE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Veggie Burger', FALSE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Double Cheeseburger', FALSE);
+
