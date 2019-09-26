02d1984abffc1362b7f427dcdfb34dfc7f73e421
Danny Kim <dudkny@gmail.com>
2019-09-25 22:26:00 -0500

    Updated server; corrected method name in controller

--- server.js
+++ server.js
@@ -2,20 +2,33 @@
 // DEPENDENCIES
 // ==============================================================================
 const express = require('express');
+const exphbs = require("express-handlebars");
 
 // ==============================================================================
 // EXPRESS CONFIGURATION
 // ==============================================================================
 
 // Declare express server usage in node
-let app = express();
+const app = express();
 
 // Sets an initial port
-var PORT = process.env.PORT || 8080;
+let PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+let routes = require("./controllers/burgers_controller");
+
+app.use(routes);
 
 // =============================================================================
 // LISTENER
